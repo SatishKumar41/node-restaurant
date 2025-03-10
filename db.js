8a6785c6d063af5781715a0f40548005c8087ac6
Satish kumar <skumar41d@gmail.com>
2025-03-10 21:58:28 +0530

    shift mongoDB to online server

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const db = mongoose.connect('mongodb://localhost:27017/mydatabase')
+const dbURL = process.env.MONGODB_URL;
+//const db = mongoose.connect('mongodb://localhost:27017/mydatabase')
+const db =mongoose.connect(dbURL);
 
 db.then(() => {
 console.log('Connected to MongoDB Server');
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db  =  require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -18,5 +19,6 @@ app.get('/', function(req, res){
     res.send("Welcome to My Hotels... ");
 })
 
+const PORT = process.env.PORT || 3001
 
-app.listen(3001, ()=>{console.log('listening on port 3001')});
+app.listen(PORT, ()=>{console.log('listening on port 3001')});
