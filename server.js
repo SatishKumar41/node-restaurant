cbbf79821eed42eb705a68650ffd684f75315b3c
Satish kumar <skumar41d@gmail.com>
2025-03-11 21:38:45 +0530

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const db  =  require('./db');
 require('dotenv').config();
+const passport =require('./auth');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -11,20 +12,24 @@ const logRequest =(req, res, next)=>{
     console.log(`[${new Date().toLocaleString()}] request made to : ${req.originalUrl}`);
     next();
 }
-
 app.use(logRequest);
+
+//Auth. function
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false});
 
 // import the router files
 const personRoutes = require('./Routes/personRoutes');
 const menuItem = require('./Routes/menuItemRoutes');
+
 //use the routers
-app.use('/person', personRoutes);
+app.use('/person', localAuthMiddleware, personRoutes);
 app.use('/menu', menuItem);
+
 
 app.get('/', function(req, res){
     res.send("Welcome to My Hotel ");
 })
 
 const PORT = process.env.PORT || 3001
-
 app.listen(PORT, ()=>{console.log('listening on port 3001')});
