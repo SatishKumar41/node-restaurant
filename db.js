69d77a7452b585121f2a737ef47aa1a490fb9a61
Satish kumar <skumar41d@gmail.com>
2025-03-11 13:34:44 +0530

    server file eddited

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const dbURL = process.env.MONGODB_URL;
+const mongodbURL = process.env.MONGODB_URL;
 //const dbURLLOCAL = process.env.MONGODB_URL;
 //const db = mongoose.connect(dbURLLOCAL)
-const db =mongoose.connect(dbURL);
-//console.log(db);
-db.then(() => {
-console.log('Connected to MongoDB Server');
-}).catch(err => {
-console.error('Error connecting to MongoDB Server', err);
-});
+mongoose.connect(mongodbURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db =mongoose.connection;
+
+db.on('connected', ()=>{console.log('Connected to MongoDB Server')})
+db.on('error', (err)=>{console.error('Error connecting to MongoDB Server', err)})
+db.on('disconnected', ()=>{console.log('Disconnected from MongoDB Server')})
 
 
 //Export a database Connection
